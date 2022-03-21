2be37c23759c7f9dfce26c8da9995710bafcd691
Anatoly <workmishiz@gmail.com>
2022-03-21 21:33:06 +0300

    Ansible init

--- devops1701.tf
+++ devops1701.tf
@@ -41,7 +41,7 @@ resource "digitalocean_droplet" "devbuild" {
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook -i '${self.ipv4_address},' --private-key ${var.pvt_key} devsrv.yml"
+    command = "ansible-playbook -u root -i '${self.ipv4_address},' --private-key ${var.pvt_key} devsrv.yml"
   }
 
 }
