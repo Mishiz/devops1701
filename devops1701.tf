25c6367462284b8b12f8100f098cb2070331018e
Anatoly <workmishiz@gmail.com>
2022-03-22 23:03:22 +0300

    Ansible 2 init

--- devops1701.tf
+++ devops1701.tf
@@ -22,9 +22,9 @@ data "digitalocean_ssh_key" "jenkins" {
   name = "jenkins"
 }
 
-resource "digitalocean_droplet" "devbuild1" {
+resource "digitalocean_droplet" "devbuild" {
   image    = "ubuntu-20-04-x64"
-  name     = "devbuild1"
+  name     = "devbuild"
   region   = "fra1"
   size     = "s-1vcpu-2gb-amd"
   ssh_keys = [
@@ -46,6 +46,33 @@ resource "digitalocean_droplet" "devbuild1" {
 
   provisioner "local-exec" {
     command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root --private-key ${var.pvt_key} -i '${self.ipv4_address_private},' -T 300 devsrv.yml"
+  }
+
+}
+
+resource "digitalocean_droplet" "stage1701" {
+  image    = "ubuntu-20-04-x64"
+  name     = "stage1701"
+  region   = "fra1"
+  size     = "s-1vcpu-2gb-amd"
+  ssh_keys = [
+    data.digitalocean_ssh_key.jenkins.id
+  ]
+  provisioner "remote-exec" {
+    inline = [
+      "apt update",
+      "apt update",
+      "apt install -y python"
+    ]
+    connection {
+      host        = self.ipv4_address
+      user        = "root"
+      type        = "ssh"
+      private_key = file(var.pvt_key)
+    }
+  }
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root --private-key ${var.pvt_key} -i '${self.ipv4_address_private},' -T 300 stage.yml"
   }
 
 }
